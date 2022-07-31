95933a98e73e0bdb8391b9bdbbb1c12aaa46d045
xhaktmchl <xhaktmchl@naver.com>
2022-07-31 20:09:30 +0900

    feat: 기본 api responsebody구현

--- src/main/java/hello/springintroduction/controller/HelloController.java
+++ src/main/java/hello/springintroduction/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,9 +15,17 @@ public class HelloController {
         return "hello";
     }
 
+    // 기본 MVC
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    // 기본 API
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name, Model model){
+        return "hello " + name;
     }
 }
